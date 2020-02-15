12e63fd74bcc36b7a31d5373abceac89cd600cb2
Matheus Carvalho <matheus9bh@hotmail.com>
2020-02-15 17:25:28 -0300

    edit-light-logic

--- src/components/juniorenterprises/JuniorEnterpriseStronger.vue
+++ src/components/juniorenterprises/JuniorEnterpriseStronger.vue
@@ -6,10 +6,10 @@
                       <div class="wrapper d-flex align-items-center media-info text-twitter">
                       </div>
                       <td class="pl-0"><h5 class="mb-0 font-weight-medium">Alto Crescimento</h5></td>
-                                            <div class="wrapper ml-auto action-bar">
-                          <div v-if=" porc_fat < porc_proj && porc_fat < porc_mem" class="badge badge-pill badge-danger">{{ porc_fat}}%</div>                          
-                          <div v-else-if=" porc_proj < porc_fat && porc_proj < porc_mem" class="badge badge-pill badge-danger">{{ porc_proj}}%</div>
-                          <div v-else class="badge badge-pill badge-danger">{{ porc_mem}}%</div>
+                          <div class="wrapper ml-auto action-bar">                         
+                          <div v-if="porc_proj <= porc_fat && porc_proj <= porc_mem" :class="getClassProj(porc_proj)">{{porc_proj}}%</div>
+                          <div v-if="porc_mem < porc_fat && porc_mem < porc_proj" :class="getClassMem(porc_mem)">{{porc_mem}}%</div>                 
+                          <div v-if="porc_fat <= porc_proj && porc_fat <= porc_mem" :class="getClassFat(porc_fat)">{{porc_fat}}%</div> 
                       </div>
                     </div>
                   </div>
@@ -21,11 +21,11 @@
                         <small>R$ {{meta_fat}} </small>
                       </div>
                       <div class="progress progress-lg mt-2">
-                        <div v-if="porc_fat >= (6 * 8.3333333)" class="progress-bar bg-success" role="progressbar" :style="{ 'width': porc_fat+'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
-                        <div v-else-if="porc_fat >= (5 * 8.3333333) && porc_fat < (6 * 8.3333333) " class="progress-bar bg-warning" role="progressbar" :style="{ 'width': porc_fat+'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
-                          <div v-else class="progress-bar bg-danger" role="progressbar" :style="{ 'width': porc_fat+'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
-
+                        <div v-if="porc_fat >= (6 * 8.333333)" class="progress-bar bg-success" role="progressbar" :style="{ 'width': porc_fat+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
+                        <div v-if="porc_fat >= (5 * 8.333333) && porc_fat < (6 * 8.333333)" class="progress-bar bg-warning" role="progressbar" :style="{ 'width': porc_fat+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
+                        <div v-if="porc_fat < (5 * 8.333333)" class="progress-bar bg-danger" role="progressbar" :style="{ 'width': porc_fat+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_fat}}%</div>
                       </div>
+                      
                     </div>
                     <div class="template-demo">
                           <h4>Projetos</h4>
@@ -34,7 +34,9 @@
                         <small> {{meta_proj}}</small>
                       </div>
                       <div class="progress progress-lg mt-2">
-                        <div class="progress-bar bg-danger" role="progressbar" :style="{ 'width': porc_proj+'%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{porc_proj}}%</div>
+                        <div v-if="porc_proj >= (6 * 8.333333)" class="progress-bar bg-success" role="progressbar" :style="{ 'width': porc_proj+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_proj}}%</div>
+                        <div v-if="porc_proj >= (5 * 8.333333) && porc_proj < (6 * 8.333333)" class="progress-bar bg-warning" role="progressbar" :style="{ 'width': porc_proj+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_proj}}%</div>
+                        <div v-if="porc_proj < (5 * 8.333333)" class="progress-bar bg-danger" role="progressbar" :style="{ 'width': porc_proj+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_proj}}%</div>
                       </div>
                     </div>
                     <div class="template-demo">
@@ -44,7 +46,9 @@
                         <small>{{mem_fat}}%</small>
                       </div>
                       <div class="progress progress-lg mt-2">
-                        <div class="progress-bar bg-warning" role="progressbar" :style="{ 'width': porc_mem+'%'}" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">{{porc_mem}}%</div>
+                         <div v-if="porc_mem >= (6 * 8.333333)" class="progress-bar bg-success" role="progressbar" :style="{ 'width': porc_mem+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_mem}}%</div>
+                        <div v-if="porc_mem >= (5 * 8.333333) && porc_mem < (6 * 8.333333)" class="progress-bar bg-warning" role="progressbar" :style="{ 'width': porc_mem+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_mem}}%</div>
+                        <div v-if="porc_mem < (5 * 8.333333)" class="progress-bar bg-danger" role="progressbar" :style="{ 'width': porc_mem+'%'}"  aria-valuemin="0" aria-valuemax="100">{{porc_mem}}%</div>
                       </div>
                     </div>
                   </div>
@@ -55,14 +59,49 @@
 
 export default {
     name: 'JuniorEnterpriseStronger',
-    props:['soma_fat','soma_proj','meta_fat', 'meta_proj', 'porc_fat', 'porc_proj','mem_fat','mem_meta','porc_mem'],
+    props:['soma_fat','soma_proj','meta_fat', 'meta_proj', 'porc_fat', 'porc_proj','mem_meta','mem_fat','porc_mem'],
     data () {
     return {
-      
     }
   },
   components:{
   },
+  methods: {
+    getClassFat(x){
+      if(x >= (6 * 8.333333))
+      {
+        return "badge badge-pill badge-success"
+      }
+      else if (x >= (5 * 8.333333) && x < (6 * 8.333333)){ 
+        return "badge badge-pill badge-warning"
+      }else if(x < (5 * 8.333333)){
+        return "badge badge-pill badge-danger"
+      }
+    },
+    getClassProj(y){
+      if(y >= (6 * 8.333333))
+      {
+        return "badge badge-pill badge-success"
+      }
+      else if (y >= (5 * 8.333333) && y < (6 * 8.333333)){ 
+        return "badge badge-pill badge-warning"
+      }else if(y < (5 * 8.333333)){
+        return "badge badge-pill badge-danger"
+      }
+    },
+    getClassMem(z){
+      console.log('teste')
+      if(z >= (6 * 8.333333))
+      {
+        return "badge badge-pill badge-success"
+      }
+      else if (z >= (5 * 8.333333) && z < (6 * 8.333333)){ 
+        return "badge badge-pill badge-warning"
+      }else if(z < (5 * 8.333333)){
+        return "badge badge-pill badge-danger"
+      }
+    },
+  }
 }
 </script>
 
