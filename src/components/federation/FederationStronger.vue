46a191b6fa4ff828c507a70e0538a37413f52b17
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-12 23:13:23 -0300

    add-billing-by-federation

--- src/components/federation/FederationStronger.vue
+++ src/components/federation/FederationStronger.vue
@@ -3,13 +3,21 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <h1>Mais fortes </h1>
+                    <div class="row">
+                        <div class="col-lg-6">
+                      <FederationBillingChart />
+                          </div>
+                           <div class="col-lg-6">
+                          </div>
+                          </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+
+import FederationBillingChart from '../federation/FederationBillingChart';
 
 export default {
     name: 'FederationStronger',
@@ -19,6 +27,7 @@ export default {
     }
   },
   components:{
+    FederationBillingChart
   },
 }
 </script>
