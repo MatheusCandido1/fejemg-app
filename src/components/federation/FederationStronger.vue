a852f8a75c40586b262df1292026010fa5ccdb71
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-12 22:37:42 -0300

    add-federation-stronger

---
+++ src/components/federation/FederationStronger.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'FederationStronger',
+    data () {
+    return {
+      data: []
+    }
+  },
+  components:{
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
