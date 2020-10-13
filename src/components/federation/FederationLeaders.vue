24b3773b0a8df26799c99d4ab264a77c0bd86e52
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-13 11:18:17 -0300

    add-lights-by-federation

--- src/components/federation/FederationLeaders.vue
+++ src/components/federation/FederationLeaders.vue
@@ -1,13 +1,58 @@
 <template>
     <div class="grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <h1>Lideres que fazem </h1>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                     <div class="col-md-3 grid-margin stretch-card">
+                <div class="card aligner-wrapper">
+                  <div class="card-body">
+                    <div class="absolute left top bottom h-100 v-strock-2 bg-info"></div>
+                    <h5 class="mb-2">Alto Crescimento</h5>
+                    <div class="d-flex align-items-center">
+                      <h1 class="font-weight-medium text-info mb-2" v-if="dataLoaded" >{{this.data.ac}}</h1>
+                    </div>
+                    
+                  </div>
                 </div>
-            </div>
+              </div>
+              <div class="col-md-3 grid-margin stretch-card">
+                <div class="card aligner-wrapper">
+                  <div class="card-body">
+                    <div class="absolute left top bottom h-100 v-strock-2 bg-success"></div>
+                    <h5 class="mb-2">Farol Verde</h5>
+                    <div class="d-flex align-items-center">
+                      <h1 class="font-weight-medium text-success mb-2" v-if="dataLoaded" >{{this.data.green}}</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 grid-margin stretch-card">
+                <div class="card aligner-wrapper">
+                  <div class="card-body">
+                    <div class="absolute left top bottom h-100 v-strock-2 bg-warning"></div>
+                    <h5 class="mb-2">Farol Amarelo</h5>
+                    <div class="d-flex align-items-center">
+                      <h1 class="font-weight-medium text-warning mb-2" v-if="dataLoaded" >{{this.data.yellow}}</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 grid-margin stretch-card">
+                <div class="card aligner-wrapper">
+                  <div class="card-body">
+                    <div class="absolute left top bottom h-100 v-strock-2 bg-danger"></div>
+                    <h5 class="mb-2">Farol Vermelho</h5>
+                    <div class="d-flex align-items-center">
+                      <h1 class="font-weight-medium text-danger mb-2" v-if="dataLoaded" >{{this.data.red}}</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              
+                </div>
+              </div>
         </div>
-    </div>
 </template>
 <script>
 
@@ -15,8 +60,36 @@ export default {
     name: 'FederationLeaders',
     data () {
     return {
-      data: []
+      data: [],
+      dataLoaded: false
     }
+  },
+  mounted(){
+    let usuarioAux = this.$store.getters.getUsuario;
+    if(usuarioAux){
+      this.usuario = this.$store.getters.getUsuario;
+      this.$http.get(this.$urlAPI+`federacao/lideres/`+new Date().getFullYear(), {"headers":{"authorization":"Bearer "+this.$store.getters.getToken}})
+      .then(response => {
+        if(response.status){
+          this.data = response.data.success_data;
+          this.dataLoaded = true;
+          console.log(response.data.success_data);
+        } 
+      })
+      .catch(e => {
+         this.$toast.error({
+            title:'Ops...',
+            message:'Erro interno. Tente novamente mais tarde',
+            position:'bottom right',
+            closeButton: false,
+            progressBar: true,
+            showDuration: 2000,
+            hideDuration: 1000,
+            timeOut: 3000
+            })
+      })
+    }
+
   },
   components:{
   },
