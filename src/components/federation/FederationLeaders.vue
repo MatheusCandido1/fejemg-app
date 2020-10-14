6d96cfeee8bbf9127d8c99eb9e96d77f7f0b43f2
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-14 13:04:06 -0300

    add-list-of-all-ejs-ac

--- src/components/federation/FederationLeaders.vue
+++ src/components/federation/FederationLeaders.vue
@@ -11,6 +11,56 @@
                     <div class="d-flex align-items-center">
                       <h1 class="font-weight-medium text-info mb-2" v-if="dataLoaded" >{{this.data.ac}}</h1>
                     </div>
+                    <v-row justify="center" v-if="this.ejs_ac.length > 0">
+    <v-dialog
+      v-model="dialog"
+      scrollable
+      max-width="600"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="#198ae3"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Ver todas
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline">
+          EJs de Alto Crescimento
+        </v-card-title>
+        <v-card-text style="height: 300px;">
+          <v-list dense>
+      <v-list-item-group
+        v-model="item"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in ejs_ac"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+          </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Fechar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
                     
                   </div>
                 </div>
@@ -23,6 +73,7 @@
                     <div class="d-flex align-items-center">
                       <h1 class="font-weight-medium text-success mb-2" v-if="dataLoaded" >{{this.data.green}}</h1>
                     </div>
+                    
                   </div>
                 </div>
               </div>
@@ -71,7 +122,19 @@ export default {
     data () {
     return {
       data: [],
-      dataLoaded: false
+      dataLoaded: false,
+      dialog: false,
+      dialog1: false,
+      dialog2: false,
+      dialog3: false,
+      item: 1,
+      item1: 1,
+      item2: 1,
+      item3: 1,
+      ejs_ac: [],
+      ejs_green: [],
+      ejs_yellow: [],
+      ejs_red: [],
     }
   },
   mounted(){
@@ -83,6 +146,10 @@ export default {
         if(response.status){
           this.data = response.data.success_data;
           this.dataLoaded = true;
+          this.ejs_ac = response.data.ejs.ac;
+          this.ejs_green = response.data.ejs.green;
+          this.ejs_yellow = response.data.ejs.yellow;
+          this.ejs_red = response.data.ejs.red;
         } 
       })
       .catch(e => {
