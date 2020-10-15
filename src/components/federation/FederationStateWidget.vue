6bb498ba9675e79800c9e508e4a5d89c2fb2bc3a
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-15 16:09:40 -0300

    add-all-state

--- src/components/federation/FederationStateWidget.vue
+++ src/components/federation/FederationStateWidget.vue
@@ -1,19 +1,73 @@
 <template>
-   <div class="col-12 col-sm-6 col-md-3 grid-margin stretch-card">
+   <div class="row" v-if="dataLoaded">
+     <div class="col-12 col-sm-6 col-md-3 grid-margin stretch-card ">
+
                 <div class="card">
                   <div class="card-body">
                     <div class="d-flex justify-content-center">
-                      <i class="mdi mdi-clock icon-lg text-primary d-flex align-items-center"></i>
+                      <i class="mdi mdi-rocket icon-lg text-primary d-flex align-items-center"></i>
                       <div class="d-flex flex-column ml-4">
                         <div class="d-flex flex-column">
-                          <p class="mb-0">Bounce rate</p>
-                          <h4 class="font-weight-bold">32.16%</h4>
+                          <p class="mb-0" style="white-space: nowrap;">Empresas Juniores</p>
+                          <h4 class="font-weight-bold">{{result.ej_quantity}}</h4>
                         </div>
-                        <small class="text-muted">65.45% on average time</small>
+                        <small class="text-muted">Meta de {{goals.ej_quantity}}</small>
                       </div>
                     </div>
                   </div>
                 </div>
+   </div>
+      <div class="col-12 col-sm-6 col-md-3 grid-margin stretch-card ">
+
+                <div class="card">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-center">
+                      <i class="mdi mdi-cash-multiple icon-md text-primary d-flex align-items-center"></i>
+                      <div class="d-flex flex-column ml-4">
+                        <div class="d-flex flex-column">
+                          <p class="mb-0" style="white-space: nowrap;">Fat. por empresário junior</p>
+                          <h4 class="font-weight-bold">R$ {{result.billing_by_member}}</h4>
+                        </div>
+                        <small class="text-muted">Meta de R$ {{goals.billing_by_member}}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                   <div class="col-12 col-sm-6 col-md-3 grid-margin stretch-card ">
+
+                <div class="card">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-center">
+                      <i class="mdi mdi-bank icon-md text-primary d-flex align-items-center"></i>
+                      <div class="d-flex flex-column ml-4">
+                        <div class="d-flex flex-column">
+                          <p class="mb-0" style="white-space: nowrap;">Nº de IES</p>
+                          <h4 class="font-weight-bold">{{result.current_ies}}</h4>
+                        </div>
+                        <small class="text-muted" style="white-space: nowrap;">Meta de {{goals.ies}}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+   <div class="col-12 col-sm-6 col-md-3 grid-margin stretch-card ">
+
+                <div class="card">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-center">
+                      <i class="mdi mdi-school icon-md text-primary d-flex align-items-center"></i>
+                      <div class="d-flex flex-column ml-4">
+                        <div class="d-flex flex-column">
+                          <p class="mb-0" style="white-space: nowrap;">Nº de IES Juniores</p>
+                          <h4 class="font-weight-bold">{{result.current_ies_junior}}</h4>
+                        </div>
+                        <small class="text-muted" style="white-space: nowrap;">Meta de {{goals.junior_ies}}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               </div>
 </template>
 <script>
@@ -22,8 +76,33 @@ export default {
     name: 'FederationImpactWidget',
     data () {
     return {
-      data: []
+      dataLoaded: false,
+      goals: [],
+      result: []
     }
+  },
+  methods: {
+  getData() {
+    let usuarioAux = this.$store.getters.getUsuario;
+    if(usuarioAux){
+      this.usuario = this.$store.getters.getUsuario;
+      this.$http.get(this.$urlAPI+`federacao/estado/indicadores/`+new Date().getFullYear(), {"headers":{"authorization":"Bearer "+this.$store.getters.getToken}})
+      .then(response => {
+        if(response.status){
+          this.goals = response.data.success_data.goal;
+          this.result = response.data.success_data.result;
+          this.dataLoaded = true;
+        }
+      })
+      .catch(e => {
+        console.log(e)
+        alert("Erro! Tente novamente mais tarde!");
+      })
+    }
+    }
+  },
+  mounted(){
+    this.getData();
   },
   components:{
   },
